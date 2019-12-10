66110c4a487d11db65fbfc46f784e9503ee80ace
derekpagenkopp <dpagenkopp@ucsb.edu>
2019-12-10 14:17:03 -0800

    Idk whats different. Probably a lot. Maybe a little. You decide.

---
+++ data/ch4_standard_script.R
@@ -0,0 +1,57 @@
+#------------------------------------------
+# Load packages
+library(tidyverse)
+library(here)
+
+
+#-------------------------------------------
+# Read in data
+
+ch4_standards <- read_csv(here::here("data",
+                                     "ch4_standard_raw.csv"))
+
+#-------------------------------------------
+# Use lm(), linear model function, to create regression model
+
+### Change dates in name and filter
+
+fit_ch4_######## <- lm(ppm ~ area, data = ch4_standards %>% filter(date == "##########"))
+
+#-------------------------------------------
+#Use summary function to get summary information about linear model
+
+### Changes dates
+
+summary(fit_ch4_########)
+
+#--------------------------------------------
+#Function to create standard curve
+### Change date in labs
+
+ggplot_regression <- function (fit) {
+  
+  require(ggplot2)
+  
+  ggplot(fit$model, aes_string(x = names(fit$model)[1], y = names(fit$model)[2])) + 
+    geom_point() +
+    stat_smooth(method = "lm", col = "red") +
+    labs(title = "######## CH4 Standard Curve", caption = paste("Adj R2 = ",signif(summary(fit)$adj.r.squared, 5),
+                                                                "Intercept =",signif(fit$coef[[1]],5 ),
+                                                                " Slope =",signif(fit$coef[[2]], 5),
+                                                                " P =",signif(summary(fit)$coef[2,4], 5)))
+}
+
+### Change date in filter
+
+
+
+
+ggplot_regression(lm(ppm ~ area, data = ch4_standards %>% 
+                       filter(date == "##########")))+
+  coord_flip()+
+  theme_classic()+
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        plot.title = element_text(hjust = 0.5),
+        title = element_text(vjust=3))
+
---
+++ data/co2_standard_20191115.R
@@ -0,0 +1,65 @@
+#------------------------------------------
+# Load packages
+library(tidyverse)
+library(here)
+
+
+#-------------------------------------------
+# Read in data
+
+co2_standards <- read_csv(here::here("data",
+                          "co2_standards_raw.csv"))
+
+#-------------------------------------------
+# Use lm(), linear model function, to create regression model
+
+### Change dates in name and filter
+
+fit20191115 <- lm(ppm ~ area, data = co2_standards %>% filter(date == "2019_11_15"))
+
+#-------------------------------------------
+#Use summary function to get summary information about linear model
+
+### Changes dates
+
+summary(fit20191115)
+
+#--------------------------------------------
+#Function to create standard curve
+### Change date in labs
+
+ggplot_regression <- function (fit) {
+  
+  require(ggplot2)
+  
+  ggplot(fit$model, aes_string(x = names(fit$model)[1], y = names(fit$model)[2])) + 
+    geom_point() +
+    stat_smooth(method = "lm", col = "red") +
+    labs(title = "20191115 CO2 Standard Curve", caption = paste("Adj R2 = ",signif(summary(fit)$adj.r.squared, 5),
+                       "Intercept =",signif(fit$coef[[1]],5 ),
+                       " Slope =",signif(fit$coef[[2]], 5),
+                       " P =",signif(summary(fit)$coef[2,4], 5)))
+}
+
+### Change date in filter
+
+
+
+
+ggplot_regression(lm(ppm ~ area, data = co2_standards %>% 
+                       filter(date == "2019_11_15")))+
+  coord_flip()+
+  theme_classic()+
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        plot.title = element_text(hjust = 0.5),
+        title = element_text(vjust=3))
+
+
+
+  
+
+
+
+      
+                
